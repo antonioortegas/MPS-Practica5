f04fafee16f82a1c688c79463c38068543c35a9b
SprenBonded <antonio22ortega@gmail.com>
2023-04-27 11:16:27 +0200

    init

---
+++ src/main/java/org/mps/boundedqueue/App.java
@@ -0,0 +1,13 @@
+package org.mps.boundedqueue;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
